164ee906324e4f418f38ea6b3d3ef605b546df07
krish214 <88020328+krish214@users.noreply.github.com>
2023-01-31 19:02:29 +0530

    sample1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Sample-ecm'`, () => {
+  it(`should have as title 'energy'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Sample-ecm');
+    expect(app.title).toEqual('energy');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Sample-ecm app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('energy app is running!');
   });
 });
